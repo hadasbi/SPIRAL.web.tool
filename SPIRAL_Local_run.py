76727bfe033e82b87e432c20b320de94ccd5ec4d
hadasbi <hadas.moriah@gmail.com>
2023-05-02 14:26:58 +0300

    Update SPIRAL_Local_run.py
    sample name index was incorrect

--- SPIRAL_Local_run.py
+++ SPIRAL_Local_run.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
         print("How are your samples called?")
         for index, val in enumerate(samples_names):
             print("{} for {}".format(index + 1, val))
-        chosen_index = int(input())
+        chosen_index = int(input()) - 1
         if 1 <= chosen_index <= len(samples_names):
             chosen_samples_names = samples_names[chosen_index]
 
